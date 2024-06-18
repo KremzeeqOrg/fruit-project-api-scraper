4bc48ca5b528efefae822a7d446504b9d5236ada
kremzeeq <sian.thompson@gmx.co.uk>
2024-06-18 16:09:07 +0100

    Feature: AWS Step Functions State Machine

--- src/handler.py
+++ src/handler.py
@@ -1,4 +1,6 @@
 import os
+#import os
+import json
 from modules.orchestrator import Orchestrator
 
 APP = os.environ.get("APP")
@@ -6,6 +8,9 @@ SOURCE_API_NAME = os.environ.get("SOURCE_API_NAME")
 
 
 def main(event, context):
+  event_body = json.loads(event['body'])
+  app = event_body["app"]
+  source_api_name = event_body['sourceApiName']
   orchestrator = Orchestrator(APP, SOURCE_API_NAME)
   orchestrator.execute()
 
