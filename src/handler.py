0c3d811183722aab885933b5c32cbea59582d9dd
Sian Thompson <41695967+Kremzeeq@users.noreply.github.com>
2024-06-18 18:08:54 +0100

    Feature: AWS Step Functions State Machine setup to chain web scraping using fruit-project-api-scraper lambda

--- src/handler.py
+++ src/handler.py
@@ -1,13 +1,18 @@
-import os
+#import os
+import json
 from modules.orchestrator import Orchestrator
 
-APP = os.environ.get("APP")
-SOURCE_API_NAME = os.environ.get("SOURCE_API_NAME")
+# for testing locally
+#app = os.environ.get("APP")
+#source_api_name = os.environ.get("SOURCE_API_NAME")
 
 
 def main(event, context):
-  orchestrator = Orchestrator(APP, SOURCE_API_NAME)
-  orchestrator.execute()
+    app = event["app"]
+    source_api_name = event['sourceApiName']
+    orchestrator = Orchestrator(app, source_api_name)
+    orchestrator.execute()
+
 
 if __name__ == '__main__':
   main('', '')
