e8866de2c6d13bddffa7f5ffb650ad755b1b457f
kremzeeq <sian.thompson@gmx.co.uk>
2024-06-18 17:11:45 +0100

    handler refactor

--- src/handler.py
+++ src/handler.py
@@ -8,15 +8,11 @@ SOURCE_API_NAME = os.environ.get("SOURCE_API_NAME")
 
 
 def main(event, context):
-  try: 
-    type_of_event = type(event)
-    # payload = json.loads(event)
     app = event["app"]
     source_api_name = event['sourceApiName']
     orchestrator = Orchestrator(app, source_api_name)
     orchestrator.execute()
-  except Exception as e:
-    raise Exception(f'{e} - event {event}')
+
 
 if __name__ == '__main__':
   main('', '')
