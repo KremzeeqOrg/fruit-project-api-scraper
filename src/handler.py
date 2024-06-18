e1ddfce157945aebb545dddc94e0020c09e2bf32
kremzeeq <sian.thompson@gmx.co.uk>
2024-06-18 16:53:08 +0100

    testing

--- src/handler.py
+++ src/handler.py
@@ -8,11 +8,14 @@ SOURCE_API_NAME = os.environ.get("SOURCE_API_NAME")
 
 
 def main(event, context):
-  payload = json.loads(event['Payload'])
-  app = payload["app"]
-  source_api_name = payload['sourceApiName']
-  orchestrator = Orchestrator(app, source_api_name)
-  orchestrator.execute()
+  try: 
+    payload = json.loads(event)
+    app = payload["app"]
+    source_api_name = payload['sourceApiName']
+    orchestrator = Orchestrator(app, source_api_name)
+    orchestrator.execute()
+  except KeyError as e:
+    raise KeyError(f'{e} - event {payload}')
 
 if __name__ == '__main__':
   main('', '')
