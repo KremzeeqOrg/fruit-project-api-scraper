f3cbbcd808313f2122a4e8988299174f9aaf8750
kremzeeq <sian.thompson@gmx.co.uk>
2024-06-18 14:34:44 +0100

    Updated handler and statemachine

--- src/handler.py
+++ src/handler.py
@@ -1,12 +1,17 @@
 import os
+import json
 from modules.orchestrator import Orchestrator
 
-APP = os.environ.get("APP")
-SOURCE_API_NAME = os.environ.get("SOURCE_API_NAME")
+# for local testing
+# app = os.environ.get("APP")
+# source_api_name = os.environ.get("SOURCE_API_NAME")
 
 
 def main(event, context):
-  orchestrator = Orchestrator(APP, SOURCE_API_NAME)
+  event_body = json.loads(event['body'])
+  app = event_body["app"]
+  source_api_name = event_body['sourceApiName']
+  orchestrator = Orchestrator(app, source_api_name)
   orchestrator.execute()
 
 if __name__ == '__main__':
