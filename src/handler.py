d40d3be389e003cfabe134215efdc793b275517b
kremzeeq <sian.thompson@gmx.co.uk>
2024-07-19 09:16:58 +0100

    Ammended handler

--- src/handler.py
+++ src/handler.py
@@ -3,7 +3,7 @@ from modules.orchestrator import Orchestrator
 # testing locally
 
 event = {"app": "fruit-project-api-scraper",
-         "sourceApiName": "fruity-vice"}
+         "sourceApiName": "fruity-vic"}
 
 def main(event, context):
     try:
@@ -12,7 +12,7 @@ def main(event, context):
       orchestrator = Orchestrator(app, source_api_name)
       orchestrator.execute()
     except Exception as e:
-       print(e)
+       print(f"{type(e).__name__} : {e}") 
        exit(1)
 
 if __name__ == '__main__':
