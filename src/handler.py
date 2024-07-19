2032be96e8041ddf09fc29f5b333b0dcc2123941
kremzeeq <sian.thompson@gmx.co.uk>
2024-07-19 08:39:39 +0100

    Testing error code handling

--- src/handler.py
+++ src/handler.py
@@ -2,16 +2,20 @@ from modules.orchestrator import Orchestrator
 
 # testing locally
 
-# event = {"app": "fruit-project-api-scraper",
-#          "sourceApiName": "fruity-vice"}
+event = {"app": "fruit-project-api-scraper",
+         "sourceApiName": "fruity-vice"}
 
 def main(event, context):
-    app = event["app"]
-    source_api_name = event['sourceApiName']
-    orchestrator = Orchestrator(app, source_api_name)
-    orchestrator.execute()
+    try:
+      app = event["app"]
+      source_api_name = event['sourceApiName']
+      orchestrator = Orchestrator(app, source_api_name)
+      orchestrator.execute()
+    except Exception as e:
+       print(e)
+       exit(1)
 
 if __name__ == '__main__':
-  # uncomment to test event payload locally
-  # main(event, '')
-  main('', '')
+  #uncomment to test event payload locally
+  main(event, '')
+  # main('', '')
