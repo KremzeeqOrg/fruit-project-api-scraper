6a8f104847cdd05519e9c2de3e8e70f4ee9247fe
Silvana <41695967+Kremzeeq@users.noreply.github.com>
2024-07-19 09:45:22 +0100

    Bug fix/add improved error handling for uncaught exception from api  mapping manager (#36)

--- src/handler.py
+++ src/handler.py
@@ -1,3 +1,4 @@
+import logging
 from modules.orchestrator import Orchestrator
 
 # testing locally
@@ -6,12 +7,16 @@ from modules.orchestrator import Orchestrator
 #          "sourceApiName": "fruity-vice"}
 
 def main(event, context):
+  try:
     app = event["app"]
     source_api_name = event['sourceApiName']
     orchestrator = Orchestrator(app, source_api_name)
     orchestrator.execute()
+  except Exception as e:
+    logging.exception(e)
+    exit(1)
 
 if __name__ == '__main__':
-  # uncomment to test event payload locally
+  #uncomment to test event payload locally
   # main(event, '')
   main('', '')
